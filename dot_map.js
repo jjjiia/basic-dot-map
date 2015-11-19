a6098826623f3c3d26c0f99d4fbcf44e92eaa9a9
Jia <zhangjia@mit.edu>
2015-11-19 15:33:13 -0500

    add some comments

--- dot_map.js
+++ dot_map.js
@@ -8,14 +8,22 @@ $(function() {
 $("#topDifferences .hideTop").hide()
 
 function dataDidLoad(error,dots,buildings) {
+//make 1 svg for everything
     var mapSvg = d3.select("#map").append("svg").attr("width",1200).attr("height",800)
+    //draw each layer
     drawBuildings(buildings,mapSvg)
-    drawPeople(dots,mapSvg)
+    //uses csv version
+    //this version of the data uses shortened, not exact lat and lngs
+    drawDots(dots,mapSvg)
 
 }
 function drawBuildings(geoData,svg){
+    //need to generalize projection into global var later
 	var projection = d3.geo.mercator().scale(4000000).center([-71.063,42.3562])
+    //d3 geo path uses projections, it is similar to regular paths in line graphs
 	var path = d3.geo.path().projection(projection);
+    
+    //push data, add path
 	svg.selectAll(".buildings")
 		.data(geoData.features)
         .enter()
@@ -26,17 +34,19 @@ function drawBuildings(geoData,svg){
 	    .style("opacity",.5)
 }
 
-function drawPeople(data,svg){
+function drawDots(data,svg){
 	var projection = d3.geo.mercator().scale(4000000).center([-71.063,42.3562])
-    svg.selectAll(".people")
+    
+    svg.selectAll(".dots")
         .data(data)
         .enter()
         .append("circle")
-        .attr("class","people")
+        .attr("class","dots")
         .attr("r",2)
         .attr("cx",function(d){
             var lat = parseFloat(d.latitude)
             var lng = parseFloat(d.longitude)
+            //to get projected dot position, use this basic formula
             var projectedLng = projection([lng,lat])[0]
             return projectedLng
         })
@@ -47,6 +57,7 @@ function drawPeople(data,svg){
             return projectedLat
         })
         .attr("fill",function(d){
+            //color code the dots by gender
             var gender = d.gender
             if(gender == "F"){
                 return "red"
@@ -57,6 +68,7 @@ function drawPeople(data,svg){
             }            
         })
 	    .style("opacity",.3)
+        //on mouseover prints dot data
         .on("mouseover",function(d){
             console.log(d)
         })
